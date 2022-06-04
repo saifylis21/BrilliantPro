61afd5ee70987b5b93e2509315aaf6f7d13dba64
Saif <saifsadiq.ss@gmail.com>
2022-06-04 12:44:50 +0500

    a dumb comment removed

--- client/src/App.js
+++ client/src/App.js
@@ -12,28 +12,7 @@ const App = () => {
       <div>
 
         <LoginPage />
-
-
-
-        {/*  show this when user is logged in */}
-        {/* <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Saif API</Typography>
-        <img className={classes.image} src={picture} alt="icon" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-        </Container> */}
+        
       </div>
     </>
   );
