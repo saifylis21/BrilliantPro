2ebddf4c34c129cd2406c216025bead7153458b2
Shaza <shaza.azher13@gmail.com>
2022-06-04 15:54:10 +0500

    styling course components

--- client/src/components/Dumb.js
+++ client/src/components/Dumb.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Dumb = () => (
-    <h1>testing1233</h1>
+    <h1>shaza is an influencer she is so cool everyone should die only shaza should live. </h1>
 );
 
 export default Dumb;
