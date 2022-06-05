99b02068e7b1f22a2b3f6da40087ba8514763a16
Saif <saifsadiq.ss@gmail.com>
2022-06-05 16:05:05 +0500

    project done

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -17,6 +17,18 @@ const Post = ({ post, setCurrentId }) => {
   const [isLoggedIn] = useGlobalState("isLoggedIn");
   const [role] = useGlobalState("role");
 
+  let showLike = <></>
+
+  if(!isLoggedIn || role === "") {
+    showLike = <></>
+  } else if(isLoggedIn && role === "Admin") {
+    showLike = <></>
+  } else if(isLoggedIn && role === "User") {
+    showLike = (
+      <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
+    )
+  }
+
   return (
       <Card className={classes.card}>
         <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
@@ -29,17 +41,19 @@ const Post = ({ post, setCurrentId }) => {
             <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}><MoreHorizIcon fontSize="default" /></Button>
           </div>
         ) : null}
-        <div className={classes.details}>
-          <Typography variant="body2" color="textSecondary" component="h2"><a href={`${post.tags}`} target="_blank" rel="noopener noreferrer">{post.tags}</a></Typography>
-        </div>
+
+        {!isLoggedIn ? (<div style={{marginTop: '10px'}}></div>) : (
+          <div className={classes.details}>
+            <Typography variant="body2" color="textSecondary" component="h2"><a href={`${post.tags}`} target="_blank" rel="noopener noreferrer">{post.tags}</a></Typography>
+          </div>
+        )}
+
         <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
         <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
         </CardContent>
         <CardActions className={classes.cardActions}>
-          {isLoggedIn && role === "Admin" ? null : (
-            <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
-          )}
+          {showLike}
           {isLoggedIn && role === "Admin" ? (
             <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
           ) : null}
